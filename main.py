87076eef82033e0261c4be9ab496f556d3637618
manishkadesilva0-create <manishkadesilva0@gmail.com>
2026-03-19 10:03:48 +0530

    A: Screen Working

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_state = 1
+
+    while game_state == 1:
+        if event.type == pygame.QUIT:
+            return
+        
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
