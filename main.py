0a3fdfce1ecd1bbe013aee566a5822a637097ed5
manishkadesilva0-create <manishkadesilva0@gmail.com>
2026-03-19 10:40:23 +0530

    B: Added player onto screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
+from player import *
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -11,6 +12,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     fps_clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player_one = Player(x, y)
 
     game_state = 1
 
@@ -20,10 +25,13 @@ def main():
                 return
         
         screen.fill("black")
+        player_one.draw(screen)
         pygame.display.flip()
         log_state()
         fps_clock.tick(60)
         dt = fps_clock.tick()/1000
+
+        
         #print(dt)
 
 
