a659031a5a71cf211ee4734d4bf6e93ca3c0a3fc
manishkadesilva0-create <manishkadesilva0@gmail.com>
2026-03-19 13:36:27 +0530

    F1: Added a shot cooldown

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
 
         self.rotation = 0
+        self.shot_countdown = 0
     
     # in the Player class
     def triangle(self):
@@ -36,7 +37,11 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.shot_countdown <= 0:
+                self.shoot()
+                self.shot_countdown = PLAYER_SHOOT_COOLDOWN_SECONDS
+        self.shot_countdown -= dt
+            
     
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
